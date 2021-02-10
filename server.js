0b1898eace525aac726eb0e387dfe76cc83b7313
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-02-09 19:16:27 -0500

    Adding functionality to post new notes to the db.json file

--- server.js
+++ server.js
@@ -27,7 +27,17 @@ app.get("*", function(req, res) {
     res.sendFile(path.join(mainDir, "index.html"));
 });
 
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
 
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
+});
 
 
 
