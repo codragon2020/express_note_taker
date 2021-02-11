aa92202be606dea547a1aaef7f4e5e6d32e4c6f6
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-02-11 06:55:51 -0500

    Updated the comments on routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,14 @@
+// Dependencies
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-
+// Routing
 const mainDir = path.join(__dirname, "/../public");
 
 module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(mainDir, "/notes.html"));
     });
@@ -19,10 +22,13 @@ module.exports = function (app) {
         res.json(savedNotes[Number(req.params.id)]);
     });
 
+    // Catch all other GET requests not defined above
     app.get("*", function(req, res) {
         res.sendFile(path.join(mainDir, "index.html"));
     });
 
+    // API POST Requests
+    // Below code handles when users "saves" a note.
     app.post("/api/notes", function(req, res) {
         let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
         let newNote = req.body;
@@ -34,6 +40,8 @@ module.exports = function (app) {
         res.json(savedNotes);
     });
 
+    // API DELETE Request
+    // Below code handles when users "deletes" a saved note.
     app.delete("/api/notes/:id", function(req, res) {
         let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
         let noteID = req.params.id;
